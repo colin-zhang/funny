a0f1576dd80d5b11d5d25ecb262b59e8f5684d63
colin-zhang <ingm@foxmail.com>
2015-01-28 15:45:18 +0800

    test java

---
+++ java-test/hello_pc_java/src/colin/math/Basic_math.java
@@ -0,0 +1,7 @@
+package colin.math;
+
+public class Basic_math{
+	public int add(int a,int b){
+		return a+b;
+	}
+}
---
+++ java-test/hello_pc_java/src/colin/test/My_test.java
@@ -0,0 +1,17 @@
+package colin.test;
+
+import colin.math.*;
+
+public class My_test {
+    
+	public static void main(String []args) {
+    	Basic_math m = new Basic_math();
+    	
+    	int a=1;
+    	int b=2;
+    	int c = m.add(a, b);
+
+    	System.out.println("Hello World");
+    	System.out.println(c);
+    }
+}
